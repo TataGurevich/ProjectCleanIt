e6ba6ac7a91b6b44d8feaecd5d9bced027dc664e
TataGurevich <127196589+TataGurevich@users.noreply.github.com>
2023-03-12 11:45:57 +0200

    убрала Routes из import {BrowserRouter, Routes} from "react-router-dom";
    писал что ошибка, не нашла где это необходимо и поэтому убрала

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
-
+//special test for git
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
