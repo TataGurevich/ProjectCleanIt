852062409d7ff5758ddec0365446fac516a5c898
TataGurevich <127196589+TataGurevich@users.noreply.github.com>
2023-03-27 20:17:40 +0300

    1) старые ненужные файлы перенесла в директорию на удаление 2) поправила грамматическую ошибку в BookNow.jsx 3) добавила два файла для клинеров 4) добавила фото клинеров

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,3 +1,7 @@
+import albina from '../../../../img/albina.png'
+import megan from '../../../../img/megan.png'
+import giselle from '../../../../img/giselle.png'
+import kate from '../../../../img/kate.png'
 export const homePage = 'Home'
 export const signUp = 'SignUp'
 export const joinOurTeam = 'JoinOurTeam'
@@ -8,3 +12,5 @@ export const regularlyPage = 'regularly'
 export const deepPage = 'deep'
 export const officePage = 'office'
 export const windowsPage = 'windows'
+
+export const cleaners = [albina, megan, giselle, kate]
--- src/components/Main/Content/Book/Cleaners.js
+++ src/components/Main/Content/Book/Cleaners.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import './cleaners.css'
+
+const Cleaners = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Cleaners;
